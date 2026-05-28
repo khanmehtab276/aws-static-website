01605c605a00944f22f6838a7cc0d87686ab2fe8
Khan Mehtab Alam <khanmahtab276@gmail.com>
2026-05-28 22:12:39 +0530

    Formated code using terraform fmt

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 module "security_group" {
-    source = "./modules/security-group"
-    sg_name = "web-app-sg"
+  source  = "./modules/security-group"
+  sg_name = "web-app-sg"
 }
 
 module "ec2" {
-    source = "./modules/ec2"
-    ami_id = var.ami_id
-    instance_type = var.instance_type
-    security_group_id = module.security_group.security_group_id
-    key_name = var.key_name
+  source            = "./modules/ec2"
+  ami_id            = var.ami_id
+  instance_type     = var.instance_type
+  security_group_id = module.security_group.security_group_id
+  key_name          = var.key_name
 }
-
--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "aws_region" {
-    description = "The AWS region to deploy resources in"
-    type = string
+  description = "The AWS region to deploy resources in"
+  type        = string
 }
 
 variable "instance_type" {
-    description = "The type of EC2 instance to deploy"
-    type = string
+  description = "The type of EC2 instance to deploy"
+  type        = string
 }
 
 variable "ami_id" {
-    description = "The AMI ID to use for the EC2 instance"
-    type = string
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
 }
 
 variable "key_name" {
-    description = "The name of the key pair to use for the EC2 instance"
-    type = string
+  description = "The name of the key pair to use for the EC2 instance"
+  type        = string
 }
